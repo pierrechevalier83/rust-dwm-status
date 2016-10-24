0b57f9c9b6848f88eeaf7639b0b6529aa5c1754f
Pierre Chevalier <pierrechevalier83@gmail.com>
2016-10-25 00:30:35 +0100

    Print the date

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use std::process::Command;
 
+extern crate chrono;
+
+fn date() -> String {
+    // 2016-10-25 00:30
+	chrono::Local::now().format("%F %R").to_string()
+}
+
 fn main() {
     Command::new("xsetroot")
 			.arg("-name")
-			.arg("Hello, World!")
+			.arg(date())
 			.spawn()
 			.expect("Failed to run command");
 }
