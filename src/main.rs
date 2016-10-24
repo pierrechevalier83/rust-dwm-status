c1c1b1d8ed06dc3888bad2110d57107802108279
Pierre Chevalier <pierrechevalier83@gmail.com>
2016-10-25 00:09:59 +0100

    Status: Hello, World!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    Command::new("xsetroot")
+			.arg("-name")
+			.arg("Hello, World!")
+			.spawn()
+			.expect("Failed to run command");
 }
